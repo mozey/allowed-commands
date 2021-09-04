903b1f3dbe85c73d7657eff673f7caffc72d5b33
Christiaan van Zyl <christiaanvanzyl@gmail.com>
2021-09-04 20:28:24 +0200

    Try to break regex with multiple commands

--- README.md
+++ README.md
@@ -5,18 +5,19 @@ Specify allowed commands in SSH authorized_keys file
 ## Usage
 
 ```bash
-SSH_ORIGINAL_COMMAND="echo foo" ./allowed-commands.sh
-# foo
-SSH_ORIGINAL_COMMAND="echo bar" ./allowed-commands.sh
-# bar
-SSH_ORIGINAL_COMMAND="echo xx" ./allowed-commands.sh
+SSH_ORIGINAL_COMMAND="ls -lah && pwd" ./allowed-commands.sh
+# ls: &&: No such file or directory
+# ls: pwd: No such file or directory
+SSH_ORIGINAL_COMMAND="ls -lah; pwd" ./allowed-commands.sh
+# ls: illegal option -- ;
+SSH_ORIGINAL_COMMAND="pwd" ./allowed-commands.sh
 # Forbidden
 ```
 
 ## Reference
 
 Inspired by the following posts
-[--1--](https://stackoverflow.com/a/402645/639133)
-[--2--](https://serverfault.com/a/803873/279654)
+- [--1--](https://stackoverflow.com/a/402645/639133)
+- [--2--](https://serverfault.com/a/803873/279654)
 
 
--- allowed-commands.sh
+++ allowed-commands.sh
@@ -1,37 +1,22 @@
 #!/usr/bin/env bash
-set -eu                   # exit on error or undefined variable
-bash -c 'set -o pipefail' # return code of first cmd to fail in a pipeline
 
-# Script fails if vars are not set in env
-# SSH_ORIGINAL_COMMAND=${SSH_ORIGINAL_COMMAND}
+set -u # exit on undefined variable
+SSH_ORIGINAL_COMMAND=${SSH_ORIGINAL_COMMAND}
 
 # These are the allowed commands
+# shellcheck disable=SC1091
 source allowed-commands.env
 
 # CMD="no match"
 
-for (( i = 0 ; i < ${#ALLOWED_COMMANDS[@]} ; i = i + 2 )) do
-    COMMAND=${ALLOWED_COMMANDS[$i]}
-    REGEX=${ALLOWED_COMMANDS[$i+1]}
-    echo "${COMMAND}"
-    echo "${REGEX}"
+for (( i = 0 ; i < ${#ALLOWED_COMMANDS[@]} ; i = i + 1 )) do
+    REGEX=${ALLOWED_COMMANDS[$i]}
+    if [[ $SSH_ORIGINAL_COMMAND =~ ${REGEX} ]] 
+    then
+        $SSH_ORIGINAL_COMMAND
+        exit $?
+    fi
 done
 
-# # Regex match
-# # https://stackoverflow.com/a/21112809/639133
-# [[ $SSH_ORIGINAL_COMMAND =~ ${CMD_FOO} ]] && CMD=${CMD_FOO}
-# [[ $SSH_ORIGINAL_COMMAND =~ ${CMD_BAR} ]] && CMD=${CMD_BAR}
-
-# # Run allowed command
-# case "$CMD" in
-#     "$CMD_FOO")
-#         $SSH_ORIGINAL_COMMAND
-#         ;;
-#     "$CMD_BAR")
-#         $SSH_ORIGINAL_COMMAND
-#         ;;
-#     *)
-#         echo "Forbidden"
-#         exit 1
-#         ;;
-# esac
+echo "Forbidden"
+exit 1
