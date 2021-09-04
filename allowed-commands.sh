62161d6633f121401a091a2b26d83d6041c54eaf
Christiaan van Zyl <christiaanvanzyl@gmail.com>
2021-09-04 15:31:36 +0200

    Env file to specify command regexes

--- allowed-commands.sh
+++ allowed-commands.sh
@@ -3,29 +3,35 @@ set -eu                   # exit on error or undefined variable
 bash -c 'set -o pipefail' # return code of first cmd to fail in a pipeline
 
 # Script fails if vars are not set in env
-SSH_ORIGINAL_COMMAND=${SSH_ORIGINAL_COMMAND}
+# SSH_ORIGINAL_COMMAND=${SSH_ORIGINAL_COMMAND}
 
 # These are the allowed commands
-CMD_FOO="^echo f.*$"
-CMD_BAR="^echo b.*$"
+source allowed-commands.env
 
-CMD="no match"
+# CMD="no match"
 
-# Regex match
-# https://stackoverflow.com/a/21112809/639133
-[[ $SSH_ORIGINAL_COMMAND =~ ${CMD_FOO} ]] && CMD=${CMD_FOO}
-[[ $SSH_ORIGINAL_COMMAND =~ ${CMD_BAR} ]] && CMD=${CMD_BAR}
+for (( i = 0 ; i < ${#ALLOWED_COMMANDS[@]} ; i = i + 2 )) do
+    COMMAND=${ALLOWED_COMMANDS[$i]}
+    REGEX=${ALLOWED_COMMANDS[$i+1]}
+    echo "${COMMAND}"
+    echo "${REGEX}"
+done
 
-# Run allowed command
-case "$CMD" in
-    "$CMD_FOO")
-        $SSH_ORIGINAL_COMMAND
-        ;;
-    "$CMD_BAR")
-        $SSH_ORIGINAL_COMMAND
-        ;;
-    *)
-        echo "Forbidden"
-        exit 1
-        ;;
-esac
+# # Regex match
+# # https://stackoverflow.com/a/21112809/639133
+# [[ $SSH_ORIGINAL_COMMAND =~ ${CMD_FOO} ]] && CMD=${CMD_FOO}
+# [[ $SSH_ORIGINAL_COMMAND =~ ${CMD_BAR} ]] && CMD=${CMD_BAR}
+
+# # Run allowed command
+# case "$CMD" in
+#     "$CMD_FOO")
+#         $SSH_ORIGINAL_COMMAND
+#         ;;
+#     "$CMD_BAR")
+#         $SSH_ORIGINAL_COMMAND
+#         ;;
+#     *)
+#         echo "Forbidden"
+#         exit 1
+#         ;;
+# esac
