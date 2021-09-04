c78fa14fbc7cfbaa057d4eeec815e4208b1add0d
Christiaan van Zyl <christiaanvanzyl@gmail.com>
2021-09-04 15:17:51 +0200

    Regex match commands

--- allowed-commands.sh
+++ allowed-commands.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/env bash
-CMD_FOO="echo foo"
-CMD_BAR="echo bar"
+set -eu                   # exit on error or undefined variable
+bash -c 'set -o pipefail' # return code of first cmd to fail in a pipeline
 
-case "$SSH_ORIGINAL_COMMAND" in
+# Script fails if vars are not set in env
+SSH_ORIGINAL_COMMAND=${SSH_ORIGINAL_COMMAND}
+
+# These are the allowed commands
+CMD_FOO="^echo f.*$"
+CMD_BAR="^echo b.*$"
+
+CMD="no match"
+
+# Regex match
+# https://stackoverflow.com/a/21112809/639133
+[[ $SSH_ORIGINAL_COMMAND =~ ${CMD_FOO} ]] && CMD=${CMD_FOO}
+[[ $SSH_ORIGINAL_COMMAND =~ ${CMD_BAR} ]] && CMD=${CMD_BAR}
+
+# Run allowed command
+case "$CMD" in
     "$CMD_FOO")
-        $CMD_FOO
+        $SSH_ORIGINAL_COMMAND
         ;;
     "$CMD_BAR")
-        $CMD_BAR
+        $SSH_ORIGINAL_COMMAND
         ;;
     *)
         echo "Forbidden"
